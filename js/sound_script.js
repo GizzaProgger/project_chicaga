37a011cc35664f26458a3ae5beb3c33d1d958d8c
unknown <147rawil147@gmail.com>
2021-05-22 14:02:44 +0400

    Показан пример как надо сделать, но он все еще требует доработки

--- js/sound_script.js
+++ js/sound_script.js
@@ -1,10 +1,25 @@
 let sound = document.querySelectorAll("[data-play-sound]");
 
 sound.forEach(snd=>{
-    snd.addEventListener('click', playsnd);
+    // Функция click принимает на вход функцию
+    // По этому ты не можешь вызвать ее так
+    // snd.addEventListener('click', playSound(snd.getAttribute("data-play-sound")));
+    // Получается, что при такой записи ты ее вызываешь и в addEventListener передаешь результат ее работы
+    // А именно undefined т.к. она ничего не возращает
+    snd.addEventListener('click', () => playSound(snd.getAttribute("data-play-sound")));
 });
 
-function playsnd(){
-    let audio = new Audio(this.getAttribute("data-play-sound"));
+function playSound(url){
+    let audio = new Audio(url);
     audio.play();
 }
+
+// Комменты не удаляй. Когда будем мержить, то удалим не нужное
+function playSoundFactory(uri) {
+    return () => playSound(uri)
+}
+
+// playSoundFactory - это фабрика по производству функций.
+// Она возращает тебе функцию. По этому можно написать так
+// snd.addEventListener('click', playSound(snd.getAttribute("data-play-sound"));
+// В данном случае это бесполезно и даже путает, но иногда такой подход может быть полезен
